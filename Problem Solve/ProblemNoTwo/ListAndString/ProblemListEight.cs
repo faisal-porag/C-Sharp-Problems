d5b00cf17de64e4b25b654eb934ec4c032ab0e2c
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2017-12-17 18:19:05 +0600

    Update ProblemListEight.cs

--- Problem Solve/ProblemNoTwo/ListAndString/ProblemListEight.cs
+++ Problem Solve/ProblemNoTwo/ListAndString/ProblemListEight.cs
@@ -28,3 +28,8 @@ namespace ListAndString
         }
     }
 }
+
+/*
+Write a function on_all that applies a function to every element of a list. Use it to print the first twenty perfect squares 
+(a natural number n is a perfect square if it can be written as n=m*m for some other natural number m. 1,4,9,16,25 are the first 5).
+*/
